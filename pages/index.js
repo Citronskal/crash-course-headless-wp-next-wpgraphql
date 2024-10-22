a2043c51234628fd17467c5ff643794a10579805
Citronskal <yiaswift13@gmail.com>
2024-10-22 13:37:11 +0200

    apollo installed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head';
 import Footer from '../components/Footer';
 import PostCard from '../components/PostCard';
-import { getAllPosts } from '../lib/test-data';
+import { client } from '../lib/apollo';
+import { gql } from "@apollo/client";
 
-
-export default function Home({ posts }) {
+export default function HomePage({ posts }) {
   return (
     <div className="container">
       <Head>
@@ -39,8 +39,32 @@ export default function Home({ posts }) {
 
 export async function getStaticProps(){
 
-  const response = await getAllPosts()
-  const posts = response?.data?.posts?.nodes
+  // Format your GraphQL query using backticks `` and prepend gql
+  const GET_POSTS = gql`
+    query AllPostsQuery {
+      posts {
+        nodes {
+          title
+          content
+          date
+          uri
+        }
+      }
+    }
+  `;
+  // Here we make a call with the client and pass in our query string to the 
+  // configuration objects 'query' property
+  const response = await client.query({
+    query: GET_POSTS
+  });
+  // Once we get the response back, we need to traverse it to pull out the 
+  // data we want to pass into the HomePage
+
+  
+  const posts = response?.data?.posts?.nodes; 
+
+  // const posts = await getAllPosts()
+
   return {
     props: {
       posts
